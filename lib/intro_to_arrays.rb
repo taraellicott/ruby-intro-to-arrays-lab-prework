3094b099231bf90f122568ca6bf78f138efecf5b
Tara Ellicott <taraellicott@gmail.com>
2018-04-11 21:07:46 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   ["apples", "oranges"]
 end  
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array, element)
+  array.push(element)
+end
+
+def last_element_with_array_methods(array, element)
+  array.pop(element)
+end
+
+def length_of_array(array)
+end
+
